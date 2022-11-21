16eaf892811da6d0d0ebedc3326422e6cdc38e78
Yelimbert <ijoselohd@gmail.com>
2022-11-21 03:09:23 -0400

    card component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([])
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(api).then(res => res.json())
+      updateFetchedData(data)
+    })()
+  }, [api])
+
   return (
     <div className="App">
-      <h1 className="text-center ubuntu">
+      <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">Wiki</span> 
         </h1>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <Filters />
+            </div>
+            <div className="col-8">
+              <div className="row">
+                <Cards results={results}/>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
