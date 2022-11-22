3b64ebf2ba80c2664878bed30c9139588e6b3cc1
Yelimbert Javier <yelimbertjavier@MacBook-Pro-de-Yelimbert.local>
2022-11-22 17:59:02 -0400

    search bar added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "bootstrap/dist/js/bootstrap"
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
   const [pageNumber, setPageNumber] = useState(1);
+  const [search, setSearch] = useState("")
   const [fetchedData, updateFetchedData] = useState([])
   const { info, results } = fetchedData;
 
-  const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
   
   useEffect(()=>{
     (async function(){
       const data = await fetch(api).then(res => res.json())
-      updateFetchedData(data)
+      updateFetchedData(data);
     })()
   }, [api])
 
@@ -24,6 +26,8 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">Wiki</span> 
         </h1>
+
+        <Search setPageNumber={setPageNumber} setSearch={setSearch} />
 
         <div className="container">
           <div className="row">
