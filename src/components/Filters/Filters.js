43bc94e6be26b28696bb6c4e2c32f4a41692cdfa
Yelimbert Javier <yelimbertjavier@MacBook-Pro-de-Yelimbert.local>
2022-11-22 23:53:48 -0400

    status modified

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Gender from "./Category/Gender";
+import Species from "./Category/Species";
+import Status from "./Category/Status";
 
 const Filters = () => {
     return (
-        <div>
-            Filters
+        <div className="col-3">
+            <div className="text-center fw-bold fs-4 mb-3">Filters</div>
+            <div style={{cursor: "pointer"}} className="text-center text-primary text-decoration-underline">Clear filters</div>
+
+            <div className="accordion" id="accordionExample">
+							<Status />
+							<Gender />
+							<Species />
+						</div>
         </div>
     );
 }
