4a3ee572fbc207941f9b0025d4c1282e7695b170
Yelimbert Javier <yelimbertjavier@MacBook-Pro-de-Yelimbert.local>
2022-11-24 22:17:57 -0400

    filters completely added

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -4,12 +4,21 @@ import Species from "./Category/Species";
 import Status from "./Category/Status";
 
 const Filters = ({ setStatus, setPageNumber, setGender, setSpecies }) => {
+
+    const clear = ()=> {
+        setStatus(""); 
+        setPageNumber("");
+        setGender(""); 
+        setSpecies("");
+        window.location.reload(false)
+    }
   return (
     <div className="col-3">
       <div className="text-center fw-bold fs-4 mb-3">Filters</div>
       <div
+        onClick={clear}
         style={{ cursor: "pointer" }}
-        className="text-center text-primary text-decoration-underline"
+        className="text-center text-primary text-decoration-underline mb-4"
       >
         Clear filters
       </div>
